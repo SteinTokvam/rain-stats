a343605642c6cf3fd495ff7efc69e9b50e4b288f
Stein Petter Tokvam <stto1210@gmail.com>
2023-07-18 10:05:29 +0200

    added footer to all pages and padding below table in dashboard

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import SignUp from './components/SignUp';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
       <div className='App'>
-        <Routes>
-          <Route path='/login' element={ <Login /> } />
-          <Route path='/register' element={ <SignUp /> } />
-          <Route path='/' element={ <Dashboard /> } />
-        </Routes>
+          <div id='content'>
+            <Routes>
+              <Route path='/login' element={ <Login /> } />
+              <Route path='/register' element={ <SignUp /> } />
+              <Route path='/' element={ <Dashboard /> } />
+            </Routes>
+          </div>
+        <Footer  />
       </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import './Footer.css';
+
+export default function Footer() {
+    return(
+        <footer id="footer">© Rain Stats {new Date().getFullYear()}</footer>
+    )
+}
