62117990334ddb113cabe84ac40a3b32a02c6e7e
Stein Petter Tokvam <stto1210@gmail.com>
2023-09-08 10:27:44 +0200

    disable rememberMe

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,24 +1,24 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { setUID } from "../actions/User";
+//import { setUID } from "../actions/User";
 
 export default function ProtectedRoute({ children }) {
 
   var uidFromSessionStorage = window.sessionStorage.getItem('uid')
   var uidFromState = useSelector(state => state.rootReducer.user.uid)
 
-  const dispatch = useDispatch();
+  //const dispatch = useDispatch();
 
-  if(!uidFromSessionStorage && !uidFromState){//sjekker om man må hente fra localstorage eller ikke
-    console.log('timeout')
-    setTimeout(200)//skittent, men venter litt i håp om at localstorage mounter innen vi kaller på den
-    const rememberMe = window.localStorage.getItem('rememberMe')
-    console.log(rememberMe)
-    console.log("setter uid fra localstorage")
-    window.sessionStorage.setItem('uid', rememberMe)
-    dispatch(setUID(rememberMe))
-    setTimeout(200)
-  }
+  //if(!uidFromSessionStorage && !uidFromState){//sjekker om man må hente fra localstorage eller ikke
+    //console.log('timeout')
+    //setTimeout(200)//skittent, men venter litt i håp om at localstorage mounter innen vi kaller på den
+    //const rememberMe = window.localStorage.getItem('rememberMe')
+    //console.log(rememberMe)
+    //console.log("setter uid fra localstorage")
+    //window.sessionStorage.setItem('uid', rememberMe)
+    //dispatch(setUID(rememberMe))
+    //setTimeout(200)
+  //}
 
   if (!uidFromSessionStorage && !uidFromState) {
     console.log('går til login fra protected')
