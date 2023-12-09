225ff2ee7de76c62233e11832e13d50423da3aba
Stein Petter Tokvam <stto1210@gmail.com>
2023-12-09 12:37:41 +0100

    Update Urls.js

--- src/utils/Urls.js
+++ src/utils/Urls.js
@@ -1,7 +1,7 @@
 export const base_url = {
     redirect_uri: 'https://steintokvam.github.io/rain-stats',
     netatmo: 'https://api.netatmo.com',
-    backend: 'https://rain-stats-serverless.vercel.app'
+    backend: 'https://rain-stats-serverless-git-new-dates-test-steintokvam.vercel.app/'//'https://rain-stats-serverless.vercel.app'
 }
 
 export const routes = {
