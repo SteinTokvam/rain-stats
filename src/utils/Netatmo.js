655e1f1ce12f29226a8f9ec3cae99b3407db1930
Stein Petter Tokvam <stto1210@gmail.com>
2024-01-16 09:41:10 +0100

    use date_begin and date_end

--- src/utils/Netatmo.js
+++ src/utils/Netatmo.js
@@ -8,8 +8,8 @@ export function getDataFromNetatmo(uid, scale, date_begin, date_end) {
             body: JSON.stringify({
                 refresh_token: !uid ? window.sessionStorage.getItem('uid') : uid,
                 scale: scale,
-                date_begin: 0,
-                date_end: date_end
+                date_begin,
+                date_end
             })
     }
     )
